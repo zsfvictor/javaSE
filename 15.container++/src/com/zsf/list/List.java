f57ecda21a020427b8f782ef64056a73a91bbb84
zsfvictor <308370939@qq.com>
2021-05-01 21:27:04 +0800

    TestArrayList

---
+++ 15.container++/src/com/zsf/list/List.java
@@ -0,0 +1,5 @@
+package com.zsf.list;
+
+public interface List {
+    //返回线性表大小，
+}
---
+++ 15.container++/src/com/zsf/list/TestArrayList.java
@@ -0,0 +1,33 @@
+package com.zsf.list;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TestArrayList {
+    public static void main(String[] args) {
+        //创建线性序列表
+        ArrayList list = new ArrayList();
+        //向末尾添加元素
+        list.add("111111");
+        list.add("aaaaaa");
+        list.add("bbbbbb");
+        list.add("333333");
+        list.add("22222");
+        list.add(3,"AAAAAAAA");
+        //通过各种操作验证添加
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        System.out.println(list.get(2));
+        System.out.println(list.contains("4444444"));
+        System.out.println(list.indexOf("22222"));
+        System.out.println(list.toString());
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()){
+            String elem = it.next();
+            System.out.println(elem);
+        }
+        List
+    }
+}
